a04a75dad39b3bfd0efef5792565ceaa86c2758a
Gopal Menon <gopal.mnn@gmail.com>
2016-03-13 11:51:07 -0600

    Method to import twitter profile data for spam and ham accounts

--- FileOperations.py
+++ FileOperations.py
@@ -1,10 +1,19 @@
 import numpy as np
 
-# Load data from a text file into an array and return the array contents
-def load_text_file(file_path, file_name):
+# Load data from a text file containing Twitter profile information into an array and return the array contents 
+# along with class label corresponding to spam or ham
+def load_twitter_profile_text_file(file_path, file_name, is_spam):
+
  try:
-  text_data = np.genfromtxt(file_path.strip() + file_name.strip(), delimiter="\t")
-  return text_data
+
+  # Array containing NumberOfFollowings, NumberOfFollowers, NumberOfTweets, LengthOfScreenName, LengthOfDescriptionInUserProfile
+  text_data = np.genfromtxt(file_path.strip() + file_name.strip(), delimiter="\t")[:, [3,4,5,6,7]].astype(int)
+
+  # Class label as a column vector or 1's or 0's corresponding to spam or ham
+  class_label = np.ones(shape=(text_data.shape[0], 1)) if is_spam else np.zeros(shape=(text_data.shape[0], 1))
+
+  return text_data, class_label
+
  except IOError as e:
   print(e)
 
