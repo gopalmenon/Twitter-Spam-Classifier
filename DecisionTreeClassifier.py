9d03e5a119726924c4e79001ede10b3d63e20689
Gopal Menon <gopal.mnn@gmail.com>
2016-03-20 17:38:47 -0600

    Added AdaBoost, Decision Tree and Logistic Regression classifiers for Twitter profile. Removed SVM classifier as it was not working.

---
+++ DecisionTreeClassifier.py
@@ -0,0 +1,21 @@
+from sklearn import tree
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+import FileOperations as fo
+
+training_data, training_data_class_labels = fo.get_twitter_profile_details(is_training_data = True, categorize_data = False)
+testing_data, testing_data_class_labels = fo.get_twitter_profile_details(is_training_data = False, categorize_data = False)
+
+classifier = tree.DecisionTreeClassifier()
+classifier.fit(training_data, training_data_class_labels)
+predicted_labels = classifier.predict(testing_data)
+print("Confusion Matrix")
+print(confusion_matrix(testing_data_class_labels, predicted_labels))
+print("Precision")
+print(precision_score(testing_data_class_labels, predicted_labels, average=None))
+print("Recall")
+print(recall_score(testing_data_class_labels, predicted_labels, average=None))
+print("F1 score")
+print(f1_score(testing_data_class_labels, predicted_labels, average=None))
