69a160947155c2a0559db09839f3eba59830cba0
Gopal Menon <gopal.mnn@gmail.com>
2016-03-26 16:12:36 -0600

    Added comments for columns being used

--- FileOperations.py
+++ FileOperations.py
@@ -8,7 +8,7 @@ TESTING_DATA_FILE_NAME = "Testing Data Tab.txt"
 def load_twitter_data_from_file(file_name):
 
  try:
-  # Get tweet data 
+  # Get tweet data for Number Of Followings, Number Of Followers, Number Of Tweets, NumberOfUrlTweets, Change in Following
   array_of_tweets = np.genfromtxt(file_name.strip(), delimiter="\t")[:, [1, 2, 3, 7, 8]].astype(int)
   array_of_data_labels = np.genfromtxt(file_name.strip(), delimiter="\t")[:, [9]].astype(int)[:, 0]
 
